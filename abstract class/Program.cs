030c9b64abe394a60e75e86a8ca0325efc35faf1
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-28 10:34:50 +0530

    first commit

---
+++ abstract class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace myApp
+{
+    abstract class Animal {
+        public abstract void animalSound();// Abstract method (does not have a body)
+        public void sleep()
+        {
+            Console.WriteLine("zzzzzzzz");
+        }
+    }
+    class Pig : Animal
+    {
+         public override void animalSound ()
+        {
+            Console.WriteLine("the pig says We Weee");
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Pig mypig = new Pig();
+            mypig.animalSound();
+            mypig.sleep();
+        }
+    }
+}
+
